a01e58a0a2be36d415d38c39334bcdc1b96d5e2d
Benidir Mourad <benidir-mourad@hotmail.com>
2025-12-03 20:31:00 +0100

    Register form

--- react_forum/src/components/modals/AuthModals.jsx
+++ react_forum/src/components/modals/AuthModals.jsx
@@ -1,9 +1,19 @@
-import { Button, Modal, Form, Input} from 'antd';
 import React, { useState } from "react";
+import { Button, Modal, Form, Input, Upload } from 'antd';
+import { UploadOutlined } from '@ant-design/icons'
 
 const AuthModals = ({ open, setOpen }) => {
     const [confirmLoading, setConfirmLoading] = useState(false);
     const [form] = Form.useForm();
+    const [switchForm, setSwitchForm] = useState(1);
+
+    const loginForm = () => {
+        setSwitchForm(1);
+    }
+
+    const registerForm = () => {
+        setSwitchForm(2);
+    }
 
     const handleCancel = () => {
         console.log('Clicked cancel button');
@@ -30,36 +40,93 @@ const AuthModals = ({ open, setOpen }) => {
                 autoComplete='off'
                 onFinish={onFinish}
             >
-                <Form.Item
-                    label="Email"
-                    name='email'
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Entrez votre e-mail'
-                        }
-                    ]}>
-                    <Input/>
-                </Form.Item>
+                {switchForm == 1 &&
+                    <>
+                        {/* Login form */}
+                        <Form.Item
+                            label="Email"
+                            name='email'
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Entrez votre e-mail'
+                                }
+                            ]}>
+                            <Input />
+                        </Form.Item>
 
-                <Form.Item
-                    label="Mot de passe"
-                    name='password'
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Entrez votre mot de passe'
-                        }
-                    ]}>
-                    <Input.Password/>
-                </Form.Item>
+                        <Form.Item
+                            label="Mot de passe"
+                            name='password'
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Entrez votre mot de passe'
+                                }
+                            ]}>
+                            <Input.Password />
+                        </Form.Item>
 
-                <p> Pas de compte ?
-                    <Button type='link'> Créer</Button>
-                </p>
+                        <p> Pas de compte ?
+                            <Button type='link' onClick={registerForm}> S'enregistrer</Button>
+                        </p>
 
+                    </>
 
+                }
 
+                {switchForm == 2 &&
+                    <>
+                        {/* Register form */}
+                        <Form.Item
+                            label="Nom et prénom"
+                            name='name'
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Entrez votre nom et prénom'
+                                }
+                            ]}>
+                            <Input />
+                        </Form.Item>
+
+                        <Form.Item
+                            label="Image de profil"
+                            name='image'>
+                            <Upload listType='picture'>
+                                <Button icon={<UploadOutlined />}>Télécharger une image de profil</Button>
+                            </Upload>
+                        </Form.Item>
+
+                        <Form.Item
+                            label="Email"
+                            name='email'
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Entrez votre e-mail'
+                                }
+                            ]}>
+                            <Input />
+                        </Form.Item>
+
+                        <Form.Item
+                            label="Mot de passe"
+                            name='password'
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Entrez votre mot de passe'
+                                }
+                            ]}>
+                            <Input.Password />
+                        </Form.Item>
+
+                        <p> Vous avez déjà un compte ?
+                            <Button type='link' onClick={loginForm}> Se connecter</Button>
+                        </p>
+                    </>
+                }
             </Form>
         </Modal>
     )
