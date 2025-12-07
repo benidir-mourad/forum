f268f30e519d2ce3102ff850c28588034abfa526
Benidir Mourad <benidir-mourad@hotmail.com>
2025-12-07 20:55:45 +0100

    Add Question Modal

---
+++ react_forum/src/components/modals/AddQuestionModal.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Modal, Form, Input } from 'antd'
+
+const AddQuestionModal = ({open, setOpen}) => {
+    const [form] = Form.useForm();
+
+    const onFinish = (values) => {
+        console.log(values);
+    }
+    return (
+        <Modal
+            title='Ajouter une question'
+            centered
+            open={open}
+            width={1000}
+            onCancel={() => setOpen(false)}
+            onOk={() => form.submit()}
+        >
+
+            <Form
+                name='basic'
+                form={form}
+                onFinish={onFinish}
+                layout='vertical'>
+                <Form.Item
+                    label="Titre"
+                    name='title'
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Entrez le titre de la question'
+                        }
+                    ]}>
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Description"
+                    name='description'
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Entrez la description de la question'
+                        }
+                    ]}>
+                    <Input.TextArea placeholder='Donner la description de votre question' />
+                </Form.Item>
+
+            </Form>
+
+        </Modal >
+    )
+}
+
+export default AddQuestionModal
--- react_forum/src/components/navbar/Navbar.jsx
+++ react_forum/src/components/navbar/Navbar.jsx
@@ -2,39 +2,44 @@ import React, { useState } from 'react'
 import { PlusOutlined } from '@ant-design/icons'
 import './navbar.css'
 import { menuItem } from '../../data'
+import AddQuestionModal from '../modals/AddQuestionModal'
 
 const Navbar = () => {
 
     const [isItemClicked, setItemClicked] = useState(null);
+    const [openAddQuestionModal, setOpenAddQuestionModal] = useState(false);
 
     const handleMenuItemClicked = (item) => {
         setItemClicked(item.id);
         console.log(item.id);
     }
 
-    return (
-        <div>
-            <nav className='widescreen-nav'>
-                <ul>
-                    <li className='add_question_btn'>
-                        <PlusOutlined />
-                        Poser une question
-                    </li>
+    const handleOpenModal = () => {
+        setOpenAddQuestionModal(true);
+    }
 
-                    {
-                        menuItem.map((item) => (
-                            <li className={isItemClicked == item.id ? 'menu_item active' : 'menu_item'}
-                                key={item.id}
-                                onClick={() => handleMenuItemClicked(item)
-                                }>
-                                <div className='menu_icon'>{item.icon}</div>
-                                <div className='menu_title'>{item.title}</div>
-                            </li>
-                        ))
-                    }
-                </ul>
-            </nav>
-        </div >
+    return (
+        <nav className='widescreen-nav'>
+            <ul>
+                <li className='add_question_btn' onClick={handleOpenModal}>
+                    <PlusOutlined />
+                    Poser une question
+                </li>
+
+                {
+                    menuItem.map((item) => (
+                        <li className={isItemClicked == item.id ? 'menu_item active' : 'menu_item'}
+                            key={item.id}
+                            onClick={() => handleMenuItemClicked(item)
+                            }>
+                            <div className='menu_icon'>{item.icon}</div>
+                            <div className='menu_title'>{item.title}</div>
+                        </li>
+                    ))
+                }
+            </ul>
+            <AddQuestionModal open={openAddQuestionModal} setOpen={setOpenAddQuestionModal}/>
+        </nav>
     )
 }
 
